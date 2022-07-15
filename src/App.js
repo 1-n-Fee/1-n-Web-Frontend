d84c0fd4b6bce6f1e62c7059a8819ee57566662e
c-jeongyyun <99jenny0614@naver.com>
2022-07-16 02:04:04 +0900

    ✨ 라우터 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/Router";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
