80137e2dbb7cccd6196c49336a9ed264e000f55b
Jeongyun Choi <77582221+c-jeongyyun@users.noreply.github.com>
2022-07-16 15:39:50 +0900

    [1/n - 44] 회원 가입 라우터 추가  (#3)
    * ✨ Id input 컴포넌트 작성
    
    * ✨ 비밀번호 input 컴포넌트 작성
    
    * ✨ 성별 입력 라디오 버튼 생성
    
    * 🎨 아이디 입력칸 옆에 @konkuk.ac.kr 추가
    
    * ✨전공 select box 컴포넌트 생성
    
    * ✨ 이름 입력 컴포넌트 생성
    
    * ✨ 닉네임 입력 컴포넌트 생성
    
    * ✨ 전화 번호 입력 컴포넌트 생성
    
    * ✨ 비밀번호 form 불충족  시 disabled 기능 추가
    
    * ✨ 비밀번호 조건 불충족 시 리셋 기능 추가
    
    * ✨ 비밀번호 입력 컴포넌트 생성
    
    * ✨ 회원 정보 입력 컴포넌트 group생성
    
    * ✨ 회원가입 페이지 생성
    
    * 🗃️  react-router-dom 라이브러리 설치
    
    * 🗃️ styled-component 라이브러리 설치
    
    * ✨라우터 컴포넌트 생성&회원가입 라우터 추가
    
    * ✨ 라우터 컴포넌트 추가
    
    * 🗃️ react-router-com, styled-component 설치
    
    * ✨ 라우터 컴포넌트 생성 및 회원가입 라우터추가
    
    * ✨ Home 컴포넌트 삭제 및 라우터 컴포넌트 추가

---
+++ src/routes/Router.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "../component/common/Header";
+import SignUpPage from "../pages/SignUpPage";
+
+const Router = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        {/* 페이지 추가하기 */}
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Router;
