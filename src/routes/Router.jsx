c099e3be5477580939067596999f56b780f38003
c-jeongyyun <99jenny0614@naver.com>
2022-07-19 01:40:08 +0900

    ✨ chat 페이지 라우터 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "../component/header/Header";
+import ChatPage from "../pages/chat/ChatPage";
 import SignUpPage from "../pages/SignUpPage";
 
 const Router = () => {
@@ -10,6 +11,7 @@ const Router = () => {
       <Routes>
         {/* 페이지 추가하기 */}
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </div>
   );
