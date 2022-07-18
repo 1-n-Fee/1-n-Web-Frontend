e523aa57ef0f5ae51fa2321e54a26fad303921b5
c-jeongyyun <99jenny0614@naver.com>
2022-07-19 01:41:26 +0900

    ✨chat page - 채팅방 메뉴 컴포넌트 추가

---
+++ src/pages/chat/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatRoomsInfo from "../../component/chat/ChatRoomsInfo";
+
+const ChatPage = () => {
+  return (
+    <div>
+      <ChatRoomsInfo />
+    </div>
+  );
+};
+
+export default ChatPage;
