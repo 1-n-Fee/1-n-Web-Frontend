19982319219c38adfbafbe09a0bdf55abd0fd605
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-19 20:11:26 +0900

    🎨 SideBar 컴포넌트 style-component로 style 수정

--- src/component/home/sideBar.jsx
+++ src/component/home/sideBar.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-//import { useEffect } from "react";
-//import MealDetail from "./meal_detail";
 import MealList from "./meal_list";
-//import styles from "./sideBar.module.css";
-
+import styled, { css } from "styled-components";
 const SideBar = ({
   meals,
   onMealClick,
@@ -12,7 +9,6 @@ const SideBar = ({
   setIsDetailOpen,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
-  //const [isDetailOpen, setIsDetailOpen] = useState(false);
   const ToggleSidebar = () => {
     isOpen === true ? setIsOpen(false) : setIsOpen(true);
   };
@@ -21,24 +17,11 @@ const SideBar = ({
   };
   return (
     <>
-      <div className="container-fluid mt-3">
-        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-md">
-          <div className="container-fluid p-2">
-            <span className="navbar-brand">Company Logo</span>
-            <div className="form-inline ml-auto">
-              <div className="btn btn-primary" onClick={ToggleSidebar}>
-                <span>CLICK</span>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
-      <div className={`sidebar ${isOpen === true ? "active" : ""}`}>
-        <div className="sd-header">
-          <div className="btn btn-primary" onClick={ToggleSidebar}>
-            <span>CLICK</span>
-          </div>
-        </div>
+      <SideBarButton onClick={ToggleSidebar}>열기</SideBarButton>
+      <SideBarWrapper isOpen={isOpen}>
+        <SideBarHeader>
+          <SideBarButton onClick={ToggleSidebar}>닫기</SideBarButton>
+        </SideBarHeader>
         <MealList
           meals={meals}
           isDetailOpen={isDetailOpen}
@@ -47,8 +30,38 @@ const SideBar = ({
           onMealClick={onMealClick}
           selectedMeal={selectedMeal}
         />
-      </div>
+      </SideBarWrapper>
     </>
   );
 };
 export default SideBar;
+
+const SideBarWrapper = styled.div`
+  width: 280px;
+  height: 100%;
+  box-shadow: 0px 4px 8px rgb(0 0 0 / 16%);
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: -100%;
+  z-index: 99;
+  transition: 0.5s;
+  ${(props) =>
+    props.isOpen &&
+    css`
+      left: 0;
+    `};
+`;
+const SideBarHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 0px 0px 15px;
+`;
+const SideBarButton = styled.button`
+  background-color: #5a8dee;
+  border: none;
+  border-radius: 0.3em;
+  color: #fff;
+  padding: 0.5em;
+`;
