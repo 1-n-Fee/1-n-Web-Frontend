103bffc62af260f4439984ecd758868f9a73a133
c-jeongyyun <99jenny0614@naver.com>
2022-07-23 22:50:40 +0900

    🎨 전역 style 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./component/common/GlobalStyles";
 import Router from "./routes/Router";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
