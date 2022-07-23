c9dfdbbf450556736bfb6201ca15de6278e3417e
c-jeongyyun <99jenny0614@naver.com>
2022-07-23 23:09:57 +0900

    🎨 전역스타일 - ul, li 기본 스타일 제거

--- src/component/common/GlobalStyles.jsx
+++ src/component/common/GlobalStyles.jsx
@@ -4,6 +4,16 @@ const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
   box-sizing: border-box;
 }
+ul{
+    list-style:none;
+    padding: 0;
+    margin:0;
+}
+li{
+    list-style:none;
+    padding: 0;
+    margin:0;
+}
 `;
 
 export default GlobalStyle;
