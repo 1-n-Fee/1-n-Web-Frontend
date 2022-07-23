b000f203156bb02065d26eac1bdb5519fee76f0b
c-jeongyyun <99jenny0614@naver.com>
2022-07-23 22:52:24 +0900

    🎨 검색창 크기, 테두리, hover 이벤트 추가

--- src/component/common/SearchBar.jsx
+++ src/component/common/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 const SearchBar = ({ onSearchListener }) => {
   const [keyword, setKeyword] = useState("");
@@ -12,10 +13,41 @@ const SearchBar = ({ onSearchListener }) => {
 
   return (
     <div>
-      <input type="text" value={keyword} onChange={onChange} />
-      <button onClick={onBtnClick}>🔎</button>
+      <Input type="text" value={keyword} onChange={onChange} />
+      <SearchBtn onClick={onBtnClick}>🔎</SearchBtn>
     </div>
   );
 };
 
 export default SearchBar;
+
+const height = 35;
+const width = 250;
+
+const Input = styled.input`
+  display: inline-block;
+  height: ${height}px;
+  width: ${width}px;
+  border-radius: ${height / 2}px 0 0 ${height / 2}px;
+  border: none;
+  border-right-width: 0px;
+  padding: 3px 30px;
+  font-size: 18px;
+  outline: none;
+  -webkit-appearance: none;
+`;
+
+const SearchBtn = styled.button`
+  margin: 10px 0 10px 0;
+  display: inline-block;
+  height: ${height + 0.3}px;
+  width: ${width / 4}px;
+  border-radius: 0 ${height / 2}px ${height / 2}px 0;
+  width: 50px;
+  border: none;
+  border-left: 0.5px dashed #ced6e0;
+  &:hover {
+    cursor: pointer;
+    background-color: #a4b0be;
+  }
+`;
