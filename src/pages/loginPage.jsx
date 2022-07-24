103843f644aa0c73febc3e70f0de3f8fedaa2cf7
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-24 16:26:13 +0900

    ✨ loginPage에 loginForm 컴포넌트 적용

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import LoginForm from "../component/login/loginForm";
 const LoginPage = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
