204e78b41568acc83e464a6935dafb5d4dbac1b3
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-24 15:56:30 +0900

    🎨 loginButton style 생성

--- src/component/login/loginButton.jsx
+++ src/component/login/loginButton.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const LoginButton = ({ onClick }) => {
-  return <button onClick={onClick}>로그인</button>;
+  return <LoginButtonWrapper onClick={onClick}>로그인</LoginButtonWrapper>;
 };
 
 export default LoginButton;
+
+const LoginButtonWrapper = styled.button`
+  width: 11.5rem;
+  height: 2.5rem;
+  margin-bottom: 0.3rem;
+  border: none;
+  border-radius: 7px;
+  background-color: #b2acfa;
+  color: white;
+`;
