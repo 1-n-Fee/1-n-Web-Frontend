12f52c0db51b25961bc4f6b0525289e6845a880f
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-24 15:55:27 +0900

    ✨ loginButton 컴포넌트 생성

---
+++ src/component/login/loginButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginButton = ({ onClick }) => {
+  return <button onClick={onClick}>로그인</button>;
+};
+
+export default LoginButton;
