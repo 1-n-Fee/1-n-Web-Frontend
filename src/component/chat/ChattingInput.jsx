c13b891edcd6c9f85352557e03778baf76180f82
c-jeongyyun <99jenny0614@naver.com>
2022-07-26 00:37:05 +0900

    🎨 채팅 input 컴포넌트 스타일 추가

--- src/component/chat/ChattingInput.jsx
+++ src/component/chat/ChattingInput.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from "react";
+import styled from "styled-components";
 
 const ChattingInput = ({ isDelivered }) => {
   const [chatText, setChatText] = useState("");
@@ -13,18 +14,66 @@ const ChattingInput = ({ isDelivered }) => {
   }, [isDelivered]);
 
   return (
-    <div>
+    <ChatInputWrapper>
       {isDelivered && (
         <div>
-          <button onClick={onExitBtnClick}>{`🔄️ 채팅방 나가기`}</button>
+          <ExitBtn onClick={onExitBtnClick}>{`🔄️ 채팅방 나가기`}</ExitBtn>
         </div>
       )}
-      <div>
-        <input type="text" onChange={onChange} value={chatText} />
-        <button>보내기</button>
-      </div>
-    </div>
+      <InputNBtnDiv>
+        <Input type="text" onChange={onChange} value={chatText} />
+        <SendBtn disabled={chatText.length === 0}>보내기</SendBtn>
+      </InputNBtnDiv>
+    </ChatInputWrapper>
   );
 };
 
 export default ChattingInput;
+const height = 50;
+const ChatInputWrapper = styled.div`
+  width: 100%;
+  padding: 0 20px;
+`;
+
+const InputNBtnDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  display: inline-block;
+  width: 90%;
+  height: ${height}px;
+  border-radius: ${height / 2}px;
+  border: 0.5px solid #ced6e0;
+  font-size: 18px;
+  padding: 0 30px;
+`;
+
+const ExitBtn = styled.button`
+  background-color: transparent;
+  margin: 10px 0;
+  &:hover {
+    background-color: transparent;
+  }
+`;
+
+const SendBtn = styled.button`
+  height: ${height - 5}px;
+  width: 75px;
+  border-radius: ${(height - 5) / 2}px;
+  font-size: 16px;
+  margin-left: 10px;
+  background-color: #f56649;
+  &:hover {
+    background-color: #f35a3c;
+  }
+  &:disabled {
+    background-color: #bdc3c7;
+    &:hover {
+      background-color: #bdc3c7;
+      cursor: default;
+    }
+  }
+`;
