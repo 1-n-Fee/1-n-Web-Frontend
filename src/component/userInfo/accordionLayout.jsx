76bd6cb84da04d263dbf6f0522b013104e72ee3e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-26 16:35:03 +0900

    ✨ accordionLayout 컴포넌트 생성

---
+++ src/component/userInfo/accordionLayout.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import styled from "styled-components";
+const AccordionLayout = ({
+  title,
+  activeIndex,
+  index,
+  children,
+  setActiveIndex,
+}) => {
+  const handleSetIndex = (index) =>
+    activeIndex !== index && setActiveIndex(index);
+
+  return (
+    <LayoutWrapper>
+      <TitleWrapper onClick={() => handleSetIndex(index)}>
+        <div>{title}</div>
+        {activeIndex !== index && (
+          <div>
+            <FontAwesomeIcon icon={solid("chevron-right")} />
+          </div>
+        )}
+      </TitleWrapper>
+      {activeIndex === index && <ChildWrapper>{children}</ChildWrapper>}
+    </LayoutWrapper>
+  );
+};
+
+export default AccordionLayout;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  border-bottom: solid rgba(1, 1, 1, 0.2);
+`;
+const LayoutWrapper = styled.div`
+  width: 600px;
+  border: solid rgba(1, 1, 1, 0.3);
+`;
+
+const ChildWrapper = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+  height: 150px;
+  justify-content: space-between;
+`;
