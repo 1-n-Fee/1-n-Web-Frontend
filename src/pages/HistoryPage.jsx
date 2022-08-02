4d4292639794d28be2295bb03eec9eec8b73f1f6
c-jeongyyun <99jenny0614@naver.com>
2022-08-02 22:55:02 +0900

    ✨ 히스토리 페이지 생성

---
+++ src/pages/HistoryPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HistoryListContainer from "../component/history/HistoryListContainer";
+
+const HistoryPage = () => {
+  return (
+    <div>
+      <HistoryListContainer />
+    </div>
+  );
+};
+
+export default HistoryPage;
