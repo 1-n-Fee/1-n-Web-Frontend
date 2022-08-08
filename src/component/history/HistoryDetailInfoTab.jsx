1ebb375b4b9f6d0da0068d44e58f7aaf432c76e7
c-jeongyyun <99jenny0614@naver.com>
2022-08-08 23:41:35 +0900

    ✨ 히스토리 상세 내역 컴포넌트 생성

---
+++ src/component/history/HistoryDetailInfoTab.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const HistoryDetailInfoTab = () => {
+  const [historyInfo, setHistoryInfo] = useState({
+    roomName: "원스테이크",
+    roomId: "455",
+    location: "신공학관 정문",
+    feePerOne: 1500,
+    totalFee: 14500,
+    totalMems: 3,
+    targetNum: 5,
+  });
+  return (
+    <div>
+      <table border={true}>
+        <tr>
+          <td>🍕 방 이름</td>
+          <td>{`${historyInfo.roomName}-${historyInfo.roomId}`}</td>
+        </tr>
+        <tr>
+          <td>📍 위치</td>
+          <td>{historyInfo.location}</td>
+        </tr>
+        <tr>
+          <td>💵 1인당 배달비</td>
+          <td>{historyInfo.feePerOne.toLocaleString()}</td>
+        </tr>
+        <tr>
+          <td>💵 총 금액</td>
+          <td>{historyInfo.totalFee.toLocaleString()}</td>
+        </tr>
+        <tr>
+          <td>👤 현재 인원</td>
+          <td>{historyInfo.totalMems}명</td>
+        </tr>
+        <tr>
+          <td>👤 목표 인원</td>
+          <td>{historyInfo.targetNum}명</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default HistoryDetailInfoTab;
