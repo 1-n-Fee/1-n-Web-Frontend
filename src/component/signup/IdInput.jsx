c4ab0b1cb6a991d852f7144c27c196aea76638e9
c-jeongyyun <99jenny0614@naver.com>
2022-08-10 08:43:44 +0900

    ✨ owner에 맞게 id 컴포넌트 변형

--- src/component/signup/IdInput.jsx
+++ src/component/signup/IdInput.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
+import EmailSelector from "./ownerSignUp/EmailSelector";
 
-const IdInput = ({ idKey, authCheckKey, setData }) => {
+const IdInput = ({ idKey, authCheckKey, setData, isOwner = false }) => {
   const [id, setId] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
   const [isAuthChecked, setIsAuthChecked] = useState(false);
   const onChange = (e) => {
     setId(e.target.value);
+    setIsAuthChecked(false);
   };
 
   const onClick = (e) => {
     // 서버에 데이터 보내기
+    if (id === "") {
+      alert("이메일 앞부분을 입력해주세요!");
+      return;
+    } else if (emailAddress === "") {
+      alert("이메일 주소를 선택해주세요!");
+      return;
+    }
 
     // 중복확인 통과 안했으면
 
@@ -19,16 +29,24 @@ const IdInput = ({ idKey, authCheckKey, setData }) => {
   useEffect(() => {
     setData((curData) => ({
       ...curData,
-      [idKey]: id,
+      [idKey]: isOwner ? `${id}@${emailAddress}` : id,
       [authCheckKey]: isAuthChecked,
     }));
-  }, [authCheckKey, id, idKey, isAuthChecked, setData]);
+  }, [authCheckKey, id, idKey, isAuthChecked, setData, emailAddress, isOwner]);
 
   return (
     <>
       <input type="text" onChange={onChange} value={id} />
-      <span>@konkuk.ac.kr</span>
-      <button onClick={onClick}>인증하기</button>
+      <span>@</span>
+      {isOwner ? (
+        <EmailSelector setEmail={setEmailAddress} />
+      ) : (
+        <span>konkuk.ac.kr</span>
+      )}
+
+      <button onClick={onClick} disabled={id.length === 0}>
+        인증하기
+      </button>
       {isAuthChecked ? <span>인증 완료!✅</span> : null}
     </>
   );
