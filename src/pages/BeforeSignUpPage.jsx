ce7d442cfc911d11c0a8b46da0d4a661c962f9c5
c-jeongyyun <99jenny0614@naver.com>
2022-08-13 12:32:40 +0900

    ✨회원가입 이전 페이지 draft 작성

---
+++ src/pages/BeforeSignUpPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BeforeSignUpPage = () => {
+  return (
+    <div>
+      <h4>회원가입</h4>
+      {/* 용재 컴포넌트 활용하기  */}
+      {/* 카카오버튼 */}
+      {/* 네이버버튼 */}
+      {/* 구글버튼 */}
+    </div>
+  );
+};
+
+export default BeforeSignUpPage;
