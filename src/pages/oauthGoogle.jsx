9c03f5122dc2b91966d028ebda75eb01cfd1f928
c-jeongyyun <99jenny0614@naver.com>
2022-08-17 01:16:11 +0900

    ✨ 구글 계정으로 서비스 가입한적 없는 경우 회원가입 페이지로 이동

--- src/pages/oauthGoogle.jsx
+++ src/pages/oauthGoogle.jsx
@@ -2,34 +2,39 @@ import React from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import oauthDataAtom from "./../recoil/oauthData/atom";
 const OauthGoogle = (props) => {
   const navigate = useNavigate();
+  const setAuthCode = useSetRecoilState(oauthDataAtom);
 
   let code = new URL(window.location.href).searchParams.get("code");
   //const auth = localStorage.getItem("user");
   useEffect(() => {
-    console.log(code);
+    console.log(`코드:${code}`);
     const fetchId = async () => {
       try {
         await axios
           .get(`http://localhost:8080/auth/google?code=${code}`)
           .then((res) => {
+            console.log("before split");
             const a = res.data.split("\n");
             console.log(a);
             if (a[0] === "구글 회원가입이 되어 있지 않은 회원입니다.") {
               const b = a[1].split("=");
               const id = b[1];
               console.log(id);
-              axios
-                .post(`http://localhost:8080/signup`, {
-                  name: "홍길동",
-                  role: "admin",
-                  accountType: "google",
-                  oauthId: id,
-                })
-                .then((res) => console.log(res))
-                .then(navigate("/"))
-                .catch((error) => console.log(error));
+
+              // axios
+              //   .post(`http://localhost:8080/signup`, {
+              //     name: "홍길동",
+              //     role: "admin",
+              //     accountType: "google",
+              //     oauthId: id,
+              //   })
+              //   .then((res) => console.log(res))
+              //   .then(navigate("/"))
+              //   .catch((error) => console.log(error));
             } else {
               console.log(res, res.headers);
               const auth = res.headers.authorization;
@@ -39,10 +44,11 @@ const OauthGoogle = (props) => {
             }
           });
       } catch (e) {
-        console.log("error");
+        alert("가입되어있지 않은 회원입니다. 회원가입 페이지로 이동합니다.");
         console.log(e.response);
-        alert("로그인 실패");
-        navigate("/login");
+        setAuthCode(code);
+        navigate("/signup/oauth/google");
+        // navigate("/login");
       }
     };
     fetchId();
