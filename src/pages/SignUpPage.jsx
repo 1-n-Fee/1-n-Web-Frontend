193f8c5f476ddbb2443871ef63c16ee76d62487f
c-jeongyyun <99jenny0614@naver.com>
2022-08-17 01:21:47 +0900

    ✨oauth 회원 가입인 경우, 다른 화면 보이도록 구현

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import SignUpInputs from "../component/signup/SignUpInputs";
+import { useRecoilValue } from "recoil";
+import oauthDataAtom from "./../recoil/oauthData/atom";
 
 const SignUpPage = () => {
+  const { site } = useParams();
+  const authCode = useRecoilValue(oauthDataAtom);
+
+  useEffect(() => {
+    console.log(site);
+  }, []);
   return (
     <div>
-      <h2>회원 가입</h2>
+      {site !== undefined ? (
+        <h2>{`${site}계정으로 회원 가입`}</h2>
+      ) : (
+        <h2>회원가입</h2>
+      )}
       <span>* 표시 항목은 필수 입력 입니다.</span>
-      <SignUpInputs />
+      {site !== undefined ? (
+        <SignUpInputs authCode={authCode} accountType={site} />
+      ) : (
+        <SignUpInputs />
+      )}
     </div>
   );
 };
