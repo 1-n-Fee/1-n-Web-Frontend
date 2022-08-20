722b1b03875d1a751976ec156ada447908453e09
c-jeongyyun <99jenny0614@naver.com>
2022-08-20 22:56:01 +0900

    ✨stomp 프로토콜을 이용하여 채팅 기능 구현

--- src/component/chat/Chatting.jsx
+++ src/component/chat/Chatting.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Client, ActivationState } from "@stomp/stompjs";
+
+import ChatBubbleWrapper from "./ChatBubbleWrapper";
+import styled from "styled-components";
+import ChattingInput from "./ChattingInput";
+
+let client = new Client();
 
 const Chatting = () => {
-  const [socketToken, setSocketTocken] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
   useEffect(() => {
+    if (client.state === ActivationState.ACTIVE) {
+      client.state = ActivationState.INACTIVE;
+    }
+
+    console.log(` messages: ${messages}`);
     getSocketToken();
   }, []);
 
@@ -13,12 +27,126 @@ const Chatting = () => {
       const response = await axios.get("http://localhost:8080/socketToken", {
         headers: { Authorization: authToken },
       });
-      setSocketTocken(response.data.socketToken);
+      const socketToken = response.data.socketToken;
+      setNickname(response.data.nickname);
+
+      console.log("소켓토큰 받아오기 성공");
+
+      await connect(socketToken);
     } catch (err) {
       alert("오류가 발생했습니다. 다시 시도해주세요.");
     }
   };
-  return <div></div>;
+
+  const connect = (socketToken) => {
+    // console.log(client.state); // 지금 다른페이지 갔다오면 상태가 0이고, 바로 이 페이지에서 그대로 있으면 2임
+    try {
+      client.brokerURL = `ws://localhost:8080/ws/chat?token=${socketToken}`;
+
+      client.onConnect = (frame) => {
+        enterChatRoom();
+        client.subscribe(`/sub/chat/room/1`, (data) => {
+          const newMessage = JSON.parse(data.body);
+          console.log(newMessage);
+          if (newMessage.type === "ENTER") return;
+          setMessages((cur) => [
+            ...cur,
+            {
+              nickname: newMessage.sender,
+              content: newMessage.content,
+              sendTime: newMessage.sendTime,
+              type: newMessage.type,
+            },
+          ]);
+        });
+      };
+
+      client.onStompError = (frame) => {
+        console.log("Broker reported error: " + frame.headers["message"]);
+        console.log("Additional details: " + frame.body);
+      };
+
+      client.activate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const enterChatRoom = () => {
+    client.publish({
+      destination: `/pub/chat/message`,
+      body: JSON.stringify({
+        content: "",
+        type: "ENTER",
+        postId: 1,
+        nickname: nickname,
+      }),
+    });
+  };
+
+  const onSendMessage = (msg) => {
+    // trying to publish a message when the broker is not connected will throw an exception
+    if (!client.connected && msg !== "") {
+      alert("Broker disconnected, can't send message.");
+      return false;
+    }
+    if (msg.length > 0) {
+      const payLoad = {
+        content: msg,
+        type: "TALK",
+        postId: 1,
+        nickname: nickname,
+      };
+
+      // You can additionally pass headers
+      client.publish({
+        destination: "/pub/chat/message",
+        body: JSON.stringify(payLoad),
+      });
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
+
+  useEffect(() => {
+    onSendMessage(message);
+  }, [message]);
+
+  return (
+    <ChatSection>
+      <BubbleWrapper>
+        {messages.map((m, key) => (
+          <ChatBubbleWrapper
+            key={`chat_bubble_${key}`}
+            nickname={m.nickname}
+            icon=""
+            content={m.content}
+            time={m.sendTime}
+            isMine={m.nickname === nickname}
+          />
+        ))}
+        {/* <button onClick={enterChatRoom}>참가하깃</button>
+      <button onClick={onSampleClick}>채팅 보내깃</button> */}
+      </BubbleWrapper>
+      <ChattingInput isDelivered={true} setMessage={setMessage} />
+    </ChatSection>
+  );
 };
 
 export default Chatting;
+
+const ChatSection = styled.div`
+  background-color: #ecf0f1;
+  height: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
+
+const BubbleWrapper = styled.div`
+  height: 400px;
+  overflow: auto;
+`;
