c67e550ea6c91a7770a2652a44098fb4d133333e
c-jeongyyun <99jenny0614@naver.com>
2022-08-20 19:57:15 +0900

    ✨ 아이콘 hover 여부 및 글씨 크기 수정 기능 추가

--- src/component/Header/Icon.jsx
+++ src/component/Header/Icon.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Icon = ({ emoji, onClick }) => {
+const Icon = ({ emoji, onClick = null, fontSize = "20px" }) => {
   return (
-    <IconWrapper onClick={onClick}>
-      <Span>{emoji}</Span>
+    <IconWrapper
+      onClick={onClick}
+      fontSize={fontSize}
+      hasHoverEffect={onClick !== null}
+    >
+      <SpanWrapper>
+        <Span>{emoji}</Span>
+      </SpanWrapper>
     </IconWrapper>
   );
 };
@@ -16,17 +22,28 @@ const IconWrapper = styled.div`
   width: 50px;
   height: 50px;
   border-radius: 25px;
+  font-size: ${({ fontSize }) => fontSize};
   border: 1px solid #ced6e0;
   margin: 3px 4px;
   background-color: white;
   text-align: center;
   &:hover {
-    background-color: #ecf0f1;
-    cursor: pointer;
+    ${({ hasHoverEffect }) =>
+      hasHoverEffect &&
+      ` background-color: #ecf0f1;
+    cursor: pointer;`}
   }
 `;
-
+const SpanWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
 const Span = styled.span`
   display: inline-block;
-  line-height: 50px;
+  height:100%
+  line-height: 100%;
 `;
