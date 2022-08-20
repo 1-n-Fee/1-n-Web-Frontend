b5e3eb713977d84c13846f52566a7dbbffdb068f
c-jeongyyun <99jenny0614@naver.com>
2022-08-20 22:57:08 +0900

    🎨 태그 구조 변경 및 스타일 수정

--- src/component/chat/ChatRoom.jsx
+++ src/component/chat/ChatRoom.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ChatRoomHeader from "./ChatRoomHeader";
 import Chatting from "./Chatting";
-import ChattingInput from "./ChattingInput";
+import styled from "styled-components";
 
 const ChatRoom = () => {
   // recoil 값에 따라 서버에서 불러오거나 기본 화면 보여주거나
   return (
-    <div>
+    <ChatRoomWrapper>
       <ChatRoomHeader
         roomName={"테이큰 커피-3030"}
         state={2}
@@ -22,12 +22,22 @@ const ChatRoom = () => {
         feePerOne={500}
         location={"신공학관 정문"}
       />
-      <div>
+      <ChatMainDiv>
         <Chatting />
-      </div>
-      <ChattingInput isDelivered={true} />
-    </div>
+      </ChatMainDiv>
+    </ChatRoomWrapper>
   );
 };
 
 export default ChatRoom;
+
+const ChatRoomWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  flex: 1;
+`;
+
+const ChatMainDiv = styled.div`
+  flex-grow: 1;
+`;
