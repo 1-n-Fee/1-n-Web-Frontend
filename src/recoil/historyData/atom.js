8d49055e076bdbb37e3083f1163a9a926c3aa9ea
c-jeongyyun <99jenny0614@naver.com>
2022-08-24 11:55:15 +0900

    💡 탭의 인덱스별 내용을 설명하는 주석 추가

--- src/recoil/historyData/atom.js
+++ src/recoil/historyData/atom.js
@@ -5,7 +5,7 @@ const historyDataAtom = atom({
   default: {
     isPopUpOpen: false,
     roomId: 0,
-    clickedTab: 0,
+    clickedTab: 0, // 0 - 세부 정보, 1 - 주문 내역 , 2 - 참여인원
     // popUp에 띄울 다른 정보들
   },
 });
