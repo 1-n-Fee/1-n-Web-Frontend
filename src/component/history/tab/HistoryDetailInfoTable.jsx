3aac106d46df48edde1b228d2119ad405ce53ee9
c-jeongyyun <99jenny0614@naver.com>
2022-08-24 12:12:32 +0900

    ✨ table 하위 태그들을 tbody 태그로 감싸 table warning 제거

--- src/component/history/tab/HistoryDetailInfoTable.jsx
+++ src/component/history/tab/HistoryDetailInfoTable.jsx
@@ -14,30 +14,32 @@ const HistoryDetailInfoTab = () => {
   return (
     <div>
       <table>
-        <tr>
-          <td>🍕 방 이름</td>
-          <td>{`${historyInfo.roomName}-${historyInfo.roomId}`}</td>
-        </tr>
-        <tr>
-          <td>📍 위치</td>
-          <td>{historyInfo.location}</td>
-        </tr>
-        <tr>
-          <td>💵 1인당 배달비</td>
-          <td>{historyInfo.feePerOne.toLocaleString()}</td>
-        </tr>
-        <tr>
-          <td>💵 총 금액</td>
-          <td>{historyInfo.totalFee.toLocaleString()}</td>
-        </tr>
-        <tr>
-          <td>👤 현재 인원</td>
-          <td>{historyInfo.totalMems}명</td>
-        </tr>
-        <tr>
-          <td>👤 목표 인원</td>
-          <td>{historyInfo.targetNum}명</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>🍕 방 이름</td>
+            <td>{`${historyInfo.roomName}-${historyInfo.roomId}`}</td>
+          </tr>
+          <tr>
+            <td>📍 위치</td>
+            <td>{historyInfo.location}</td>
+          </tr>
+          <tr>
+            <td>💵 1인당 배달비</td>
+            <td>{historyInfo.feePerOne.toLocaleString()}</td>
+          </tr>
+          <tr>
+            <td>💵 총 금액</td>
+            <td>{historyInfo.totalFee.toLocaleString()}</td>
+          </tr>
+          <tr>
+            <td>👤 현재 인원</td>
+            <td>{historyInfo.totalMems}명</td>
+          </tr>
+          <tr>
+            <td>👤 목표 인원</td>
+            <td>{historyInfo.targetNum}명</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
