a3a4b148dace2df5e1fa74a08f8889b76401eb37
c-jeongyyun <99jenny0614@naver.com>
2022-08-24 12:27:25 +0900

    ✨ history recoil에 클릭한 방 세부 정보 데이터 추가

--- src/recoil/historyData/atom.js
+++ src/recoil/historyData/atom.js
@@ -7,6 +7,14 @@ const historyDataAtom = atom({
     roomId: 0,
     clickedTab: 0, // 0 - 세부 정보, 1 - 주문 내역 , 2 - 참여인원
     // popUp에 띄울 다른 정보들
+    roomName: "",
+    totalMems: 0,
+    targetNum: 0,
+    state: 0,
+    isChief: "",
+    feePerOne: 0,
+    totalFee: 0,
+    location: "",
   },
 });
 
