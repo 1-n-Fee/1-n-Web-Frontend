7c673b6b1128d09f31d4b2b38c57237959e41f07
c-jeongyyun <99jenny0614@naver.com>
2022-08-27 20:24:27 +0900

    ✨ color관련 상수  작성

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+export const COLOR = {
+  HEADER_BG: "#ffc15c",
+  YELLOW: "#fa983a",
+  RED_PINK: "#eb4d4b",
+  NAVY: "#130f40",
+  WHITE: "white",
+  GREEN: "#44bd32",
+  DARK_YELLOW: "#f7b731",
+  DARK_GREEN: "#16a085",
+  DARK_RED: "#c0392b",
+  OTHERS_BUBBLE: "#ffc15c",
+  MY_BUBBLE: "white",
+};
