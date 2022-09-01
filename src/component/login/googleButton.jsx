63640e45d999974ef9a3c4083760aa43dbf6308d
c-jeongyyun <99jenny0614@naver.com>
2022-09-01 20:26:47 +0900

    ✨ 회원가입용 oauth 버튼 컴포넌트 적용

--- src/component/login/googleButton.jsx
+++ src/component/login/googleButton.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-const GoogleButton = (props) => {
+import { COLOR } from "./../../constants/colors";
+import CustomOauthBtn from "./CustomOauthBtn";
+const GoogleButton = ({ isSignUp = false, width, height, radius }) => {
   return (
     <ButtonWrapper href="https://accounts.google.com/o/oauth2/v2/auth?client_id=63234602229-u49a73vg77uch12osuh9egbp6vmshsps.apps.googleusercontent.com&redirect_uri=http://localhost:3000/auth/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile">
-      <ImgWrapper src="google_btn.png" />
+      {isSignUp ? (
+        <>
+          <CustomOauthBtn
+            width={width}
+            height={height}
+            radius={radius}
+            text={"구글 회원가입"}
+            color={COLOR.NAVY}
+            bgColor={COLOR.WHITE}
+            borderColor={COLOR.GOOGLE_SKYBLUE}
+            icon={"googleIcon.png"}
+            iconWidth={"30px"}
+          />
+        </>
+      ) : (
+        <ImgWrapper src="google_btn.png" />
+      )}
     </ButtonWrapper>
   );
 };
@@ -12,6 +30,7 @@ export default GoogleButton;
 
 const ButtonWrapper = styled.a`
   margin: 5px;
+  text-decoration: none;
 `;
 const ImgWrapper = styled.img`
   width: 12rem;
--- src/component/login/naverButton.jsx
+++ src/component/login/naverButton.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-const NaverButton = (props) => {
+import { COLOR } from "./../../constants/colors";
+import CustomOauthBtn from "./CustomOauthBtn";
+const NaverButton = ({ isSignUp = false, width, height, radius }) => {
   return (
     <ButtonWrapper href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=uD5deNWJiPwEgGJY2FO7&redirect_uri=http://localhost:3000/auth/naver&state=1234">
-      <ImgWrapper src="naver_btn.png" />
+      {isSignUp ? (
+        <CustomOauthBtn
+          width={width}
+          height={height}
+          radius={radius}
+          text={"네이버 회원가입"}
+          color={COLOR.WHITE}
+          bgColor={COLOR.NAVER_GREEN}
+          icon={"naverIcon.png"}
+          iconWidth={"40px"}
+        />
+      ) : (
+        <ImgWrapper src="naver_btn.png" />
+      )}
     </ButtonWrapper>
   );
 };
@@ -12,6 +27,7 @@ export default NaverButton;
 
 const ButtonWrapper = styled.a`
   margin: 5px;
+  text-decoration: none;
 `;
 const ImgWrapper = styled.img`
   width: 11.5rem;
