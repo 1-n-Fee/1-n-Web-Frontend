b5db317bccd344e7831d03f26887b858b3eb4a62
c-jeongyyun <99jenny0614@naver.com>
2022-09-02 00:44:58 +0900

    🎨 성별 입력 컴포넌트 스타일 추가

--- src/component/signup/GenderRadioBtn.jsx
+++ src/component/signup/GenderRadioBtn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 const GenderRadioBtn = ({ genderKey, genderData, setData, data }) => {
   const { male, female } = genderData;
@@ -6,27 +7,42 @@ const GenderRadioBtn = ({ genderKey, genderData, setData, data }) => {
     setData((cur) => ({ ...cur, [genderKey]: e.target.value }));
   };
   return (
-    <>
-      <input
-        type="radio"
-        id={male}
-        name="gender"
-        value={male}
-        onChange={onChange}
-        checked={data[genderKey] === male}
-      />
-      <label htmlFor={male}>남성</label>
-      <input
-        type="radio"
-        id={female}
-        name="gender"
-        value={female}
-        onChange={onChange}
-        checked={data[genderKey] === female}
-      />
-      <label htmlFor={female}>여성</label>
-    </>
+    <GenderRadioBtnWrapper>
+      <div>
+        <input
+          type="radio"
+          id={male}
+          name="gender"
+          value={male}
+          onChange={onChange}
+          checked={data[genderKey] === male}
+        />
+        <Label htmlFor={male}>남성</Label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id={female}
+          name="gender"
+          value={female}
+          onChange={onChange}
+          checked={data[genderKey] === female}
+        />
+        <Label htmlFor={female}>여성</Label>
+      </div>
+    </GenderRadioBtnWrapper>
   );
 };
 
 export default GenderRadioBtn;
+
+const GenderRadioBtnWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  margin: 0 12px 0 4px;
+`;
