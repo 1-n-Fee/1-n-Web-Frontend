3a4d955992419f266eac1b1fcce4c1cb10db514e
c-jeongyyun <99jenny0614@naver.com>
2022-09-02 00:51:59 +0900

    ✨ 회원가입 경고문구, 버튼 스타일 컴포넌트 생성

--- src/component/style/SignUpStyle.jsx
+++ src/component/style/SignUpStyle.jsx
@@ -14,15 +14,38 @@ export const SignUpSelectStyle = styled.select`
 
 export const SignUpInputStyle = styled.input`
   display: inline-block;
-  width: ${({ width }) => (width === undefined ? "130px" : width)};
+  width: ${({ width }) => (width === undefined ? "230px" : width)};
   border: none;
   outline: none;
   border-bottom: 1px solid ${COLOR.DARKER_GRAY};
   height: 30px;
-  padding: 0 12px;
+  padding: 0 6px;
   font-size: 16px;
 `;
 
-export const SignUpWarningStyle = styled.input`
+export const SignUpWarningStyle = styled.span`
   display: inline-block;
+  color: ${COLOR.DARKER_GRAY};
+  font-size: 12px;
+  padding: 4px;
 `;
+
+export const SignUpCheckBtnStyle = styled.button`
+  margin: 0 5px;
+  height: 30px;
+  border-radius: 6px;
+  font-weight: 600;
+  background-color: ${COLOR.WHITE};
+  color: ${COLOR.NAVY};
+  &:hover:enabled {
+    background-color: ${COLOR.RED_PINK};
+    color: ${COLOR.NAVY};
+  }
+  &:enabled {
+    border: 2px solid ${COLOR.RED_PINK};
+  }
+`;
+
+// &:disabled {
+//   background-color: ${COLOR.LIGHT_GRAY};
+// }
