61d9aa6517e8471f0cfdadcc4fed2a5d2d849f04
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-09-02 00:53:45 +0900

    💄 mealItem UI 수정

--- src/component/home/meal_item.jsx
+++ src/component/home/meal_item.jsx
@@ -1,28 +1,78 @@
 import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const MealItem = ({ meal, onMealClick, ToggleDetailBar, isDetailOpen }) => {
   return (
-    <li
+    <LiWrapper
+      style={{ cursor: "pointer" }}
       onClick={() => {
         console.log("onCLick");
-        onMealClick(meal);
+        onMealClick(meal.postId);
         if (!isDetailOpen) {
           ToggleDetailBar();
         }
       }}
     >
-      <div>
-        <p>{meal.title}</p>
-        <span>1/3</span>
-        <span>미참여</span>
-      </div>
-      <ul>
-        <li>마감시간</li>
-        <li>장소</li>
-        <li>배달비</li>
-      </ul>
-    </li>
+      <DivWrapper>
+        <StoreWrapper>{meal.storeName}</StoreWrapper>
+        <CurrentWrapper>
+          <FontAwesomeIcon icon={solid("user")} />
+          {meal.currentNumber}/{meal.limitNumber}
+        </CurrentWrapper>
+        {meal.state === "OWNER" && <OwnerWrapper>{meal.state}</OwnerWrapper>}
+        {!meal.state && <StateWrapper>미참여</StateWrapper>}
+      </DivWrapper>
+      <InfoWrapper>
+        <li>마감시간 : {meal.closeTime}</li>
+        <li>배달비: {meal.deliveryFee}</li>
+      </InfoWrapper>
+    </LiWrapper>
   );
 };
 
 export default MealItem;
+
+const InfoWrapper = styled.ul`
+  padding: 0 7px;
+  margin-top: 1rem;
+`;
+const DivWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+const LiWrapper = styled.li`
+  width: 100%;
+  height: 10rem;
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  padding 0.3rem 0;
+`;
+
+const StoreWrapper = styled.span`
+  display: block;
+  font-weight: 700;
+  font-size: 1.2rem;
+  width: 60%;
+`;
+const CurrentWrapper = styled.span`
+  font-size: 0.7rem;
+  display: flex;
+  align-items: center;
+  gap: 2px;
+`;
+const StateWrapper = styled.span`
+  font-size: 0.8rem;
+  display: block;
+  text-align: center;
+  line-height: 1.7rem;
+  width: 3.4rem;
+  height: 1.7rem;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 1rem;
+`;
+const OwnerWrapper = styled(StateWrapper)`
+  background-color: lightseagreen;
+`;
