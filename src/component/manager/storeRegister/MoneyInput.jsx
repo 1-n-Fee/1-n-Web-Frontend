61adc4c2b75292da0421e6272d153e33bd13f7b4
c-jeongyyun <99jenny0614@naver.com>
2022-09-01 13:45:04 +0900

    ♻️ 변수명 일반화 (deliveryFee-> money)

--- src/component/manager/storeRegister/MoneyInput.jsx
+++ src/component/manager/storeRegister/MoneyInput.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const DeliveryFeeInput = ({ deliveryFee, setDeliveryFee }) => {
+const MoneyInput = ({ money, setMoney }) => {
   const onDeliveryFeeChange = (e) => {
     const value = e.target.value;
-    if (value.length !== 0)
-      setDeliveryFee(parseInt(value.replace(/[^0-9]/gi, "")));
-    else setDeliveryFee(0);
+    if (value.length !== 0) setMoney(parseInt(value.replace(/[^0-9]/gi, "")));
+    else setMoney(0);
   };
 
   return (
     <>
       <input
         type="text"
-        value={deliveryFee.toLocaleString()}
+        value={money.toLocaleString()}
         onChange={onDeliveryFeeChange}
       />
       <span>원</span>
@@ -21,9 +20,9 @@ const DeliveryFeeInput = ({ deliveryFee, setDeliveryFee }) => {
   );
 };
 
-DeliveryFeeInput.propTypes = {
+MoneyInput.propTypes = {
   deliveryFee: PropTypes.number.isRequired,
   setDeliveryFee: PropTypes.func.isRequired,
 };
 
-export default DeliveryFeeInput;
+export default MoneyInput;
