fd1291aa4dced57a1eda849b0754e9e2d43bd2be
c-jeongyyun <99jenny0614@naver.com>
2022-09-03 00:13:54 +0900

    🎨 가게 등록 페이지 스타일 수정

--- src/pages/manager/StoreRegisterPage.jsx
+++ src/pages/manager/StoreRegisterPage.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import RegisterInputs from "../../component/manager/storeRegister/RegisterInputs";
+import Underline from "./../../component/common/Underline";
 
 const StoreRegisterPage = () => {
   return (
-    <div>
-      <h2>가게 등록하기</h2>
+    <StoreRegisterPageWrapper>
+      <H2>가게 등록하기</H2>
+      <Underline width={"700px"} />
       <RegisterInputs />
-    </div>
+    </StoreRegisterPageWrapper>
   );
 };
 
 export default StoreRegisterPage;
+
+const StoreRegisterPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 0 0 0;
+`;
+
+const H2 = styled.h2`
+  padding: 0 0 10px 0;
+`;
