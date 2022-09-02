850cfdbdb22c6b431351b603cbb582e9f26b5251
c-jeongyyun <99jenny0614@naver.com>
2022-09-02 10:32:52 +0900

    🎨 히스토리 팝업 - 상세정보 탭의 테이블 스타일 추가

--- src/component/history/tab/HistoryDetailInfoTable.jsx
+++ src/component/history/tab/HistoryDetailInfoTable.jsx
@@ -1,42 +1,65 @@
 import React, { useState } from "react";
 import { historyDataAtom } from "./../../../recoil/historyData/atom";
 import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { COLOR } from "./../../../constants/colors";
 
 const HistoryDetailInfoTab = () => {
   const historyData = useRecoilValue(historyDataAtom);
 
   return (
-    <div>
-      <table>
+    <HistoryDetailInfoTabWrapper>
+      <Table>
         <tbody>
           <tr>
-            <td>🍕 방 이름</td>
-            <td>{historyData.roomName}</td>
+            <Th>🍕 방 이름</Th>
+            <Td>{historyData.roomName}</Td>
           </tr>
           <tr>
-            <td>📍 위치</td>
-            <td>{historyData.location}</td>
+            <Th>📍 위치</Th>
+            <Td>{historyData.location}</Td>
           </tr>
           <tr>
-            <td>💵 1인당 배달비</td>
-            <td>{historyData.feePerOne.toLocaleString()}</td>
+            <Th>💵 1인당 배달비</Th>
+            <Td>{historyData.feePerOne.toLocaleString()}원</Td>
           </tr>
           <tr>
-            <td>💵 총 금액</td>
-            <td>{historyData.totalFee.toLocaleString()}</td>
+            <Th>💵 총 금액</Th>
+            <Td>{historyData.totalFee.toLocaleString()}원</Td>
           </tr>
           <tr>
-            <td>👤 현재 인원</td>
-            <td>{historyData.totalMems}명</td>
+            <Th>👤 현재 인원</Th>
+            <Td>{historyData.totalMems}명</Td>
           </tr>
           <tr>
-            <td>👤 목표 인원</td>
-            <td>{historyData.targetNum}명</td>
+            <Th>👤 목표 인원</Th>
+            <Td>{historyData.targetNum}명</Td>
           </tr>
         </tbody>
-      </table>
-    </div>
+      </Table>
+    </HistoryDetailInfoTabWrapper>
   );
 };
 
 export default HistoryDetailInfoTab;
+
+const HistoryDetailInfoTabWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Table = styled.table`
+  width: 700px;
+  padding-top: 12px;
+`;
+
+const Th = styled.th`
+  border-right: 1px dashed ${COLOR.DARK_GRAY};
+  padding: 10px 0;
+`;
+
+const Td = styled.td`
+  padding: 10px 20px;
+`;
