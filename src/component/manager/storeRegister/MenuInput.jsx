968f174427456a68b8f560f450677705a372b0e4
c-jeongyyun <99jenny0614@naver.com>
2022-09-03 00:06:13 +0900

    🎨 menu input과 menu 컴포넌트 스타일 수정

--- src/component/manager/storeRegister/MenuInput.jsx
+++ src/component/manager/storeRegister/MenuInput.jsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import { COLOR } from "./../../../constants/colors";
 import { useCallback } from "react";
 import Thumbnail from "./Thumbnail";
+import { SignUpInputStyle } from "../../style/SignUpStyle";
 
 const MenuInput = ({ setMenus }) => {
   const [imgUrl, setImgUrl] = useState("");
@@ -85,7 +86,8 @@ const MenuInput = ({ setMenus }) => {
       <InputsWrapper>
         <div>
           {/* <InputTitle>메뉴명</InputTitle> */}
-          <input
+          <SignUpInputStyle
+            width={"150px"}
             placeholder="메뉴명"
             type="text"
             value={menuName}
@@ -128,6 +130,7 @@ const ImageInput = styled.div`
   width: 70px;
   height: 70px;
   border-radius: 4px;
+  margin: 0px 8px 0 0;
   background-color: ${COLOR.LIGHT_GRAY};
   &:hover {
     cursor: pointer;
@@ -144,6 +147,7 @@ const AddMenuBtn = styled.button`
   width: 30px;
   height: 30px;
   border-radius: 50%;
+  margin: 0 8px;
   border: 2px dashed ${COLOR.DARKER_GRAY};
   &:hover {
     background-color: ${COLOR.LIGHT_GRAY};
