1db05efb71d87a3855a292ec132af1cf95736cfb
c-jeongyyun <99jenny0614@naver.com>
2022-09-02 13:40:28 +0900

    ✨ 사업자 라우터에 헤더 추가

--- src/routes/ManagerRouter.jsx
+++ src/routes/ManagerRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import ManagerHeader from "../component/header/ManagerHeader";
 import ManagerHomePage from "../pages/manager/ManagerHomePage";
 import StoreRegisterPage from "../pages/manager/StoreRegisterPage";
 
 const ManagerRouter = () => {
   return (
     <div>
+      <ManagerHeader />
       <Routes>
         <Route path="/register" element={<StoreRegisterPage />} />
         <Route path="/*" element={<ManagerHomePage />} />
