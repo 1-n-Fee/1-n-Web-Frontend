4dd8aa0d25f690530a196193178b48349f154fd4
c-jeongyyun <99jenny0614@naver.com>
2022-09-03 10:51:21 +0900

    ✨ 메뉴 컴포넌트 외부 클릭 시 드롭다운 메뉴 닫히는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./component/style/GlobalStyles";
 import Router from "./routes/Router";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import ROLE from "./constants/role";
 import ManagerRouter from "./routes/ManagerRouter";
 import loginAndRoleDataAtom from "./recoil/loginAndRole/atom";
+import isMenuOpenAtom from "./recoil/menu/atom";
+import styled from "styled-components";
 
 function App() {
   const loginData = useRecoilValue(loginAndRoleDataAtom);
+  const setIsMenuOpen = useSetRecoilState(isMenuOpenAtom);
+  const onClick = (e) => {
+    setIsMenuOpen((cur) => ({ ...cur, myPage: false }));
+  };
   return (
-    <div className="App">
+    <AppWrapper className="App" onClick={onClick}>
       <GlobalStyles />
       <BrowserRouter>
         {loginData.isLogin && loginData.role === ROLE.MANAGER ? (
@@ -18,8 +24,12 @@ function App() {
           <Router />
         )}
       </BrowserRouter>
-    </div>
+    </AppWrapper>
   );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  min-height: 100vh;
+`;
