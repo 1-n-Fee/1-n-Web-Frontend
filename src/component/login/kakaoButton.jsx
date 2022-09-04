2e1a1d18c29bd0f7833534ba1a090da0fd881e03
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-09-04 20:09:40 +0900

    ✨ kakao,naver,google Button 컴포넌트 type prop 추가

--- src/component/login/kakaoButton.jsx
+++ src/component/login/kakaoButton.jsx
@@ -2,7 +2,13 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { COLOR } from "./../../constants/colors";
 import CustomOauthBtn from "./CustomOauthBtn";
-const KakaoButton = ({ isSignUp = false, width, height, radius }) => {
+const KakaoButton = ({
+  type = "student",
+  isSignUp = false,
+  width,
+  height,
+  radius,
+}) => {
   useEffect(() => {
     if (isSignUp) {
       localStorage.setItem("isSignUp", "true");
@@ -10,26 +16,49 @@ const KakaoButton = ({ isSignUp = false, width, height, radius }) => {
   }, []);
   return (
     <>
-      <ButtonWrapper
-        //href="https://kauth.kakao.com/oauth/authorize?client_id=437bc2fb95b24ca5a80d5763e4619f54&redirect_uri=http://localhost:8080/oauth/kakao/callback&response_type=code"
-        href="https://kauth.kakao.com/oauth/authorize?client_id=437bc2fb95b24ca5a80d5763e4619f54&redirect_uri=http://localhost:3000/auth/kakao&response_type=code"
-        //onClick={onKakaoConnect}
-      >
-        {isSignUp ? (
-          <CustomOauthBtn
-            width={width}
-            height={height}
-            radius={radius}
-            text={"카카오 회원가입"}
-            color={COLOR.KAKAO_BROWN}
-            bgColor={COLOR.KAKAO_YELLOW}
-            icon={"kakaoIcon.png"}
-            iconWidth={"40px"}
-          />
-        ) : (
-          <ImgWrapper src="kakao_btn.png" />
-        )}
-      </ButtonWrapper>
+      {type === "student" ? (
+        <ButtonWrapper
+          //href="https://kauth.kakao.com/oauth/authorize?client_id=437bc2fb95b24ca5a80d5763e4619f54&redirect_uri=http://localhost:8080/oauth/kakao/callback&response_type=code"
+          href="https://kauth.kakao.com/oauth/authorize?client_id=437bc2fb95b24ca5a80d5763e4619f54&redirect_uri=http://localhost:3000/auth/kakao&response_type=code"
+          //onClick={onKakaoConnect}
+        >
+          {isSignUp ? (
+            <CustomOauthBtn
+              width={width}
+              height={height}
+              radius={radius}
+              text={"카카오 회원가입"}
+              color={COLOR.KAKAO_BROWN}
+              bgColor={COLOR.KAKAO_YELLOW}
+              icon={"kakaoIcon.png"}
+              iconWidth={"40px"}
+            />
+          ) : (
+            <ImgWrapper src="kakao_btn.png" />
+          )}
+        </ButtonWrapper>
+      ) : (
+        <ButtonWrapper
+          //href="https://kauth.kakao.com/oauth/authorize?client_id=437bc2fb95b24ca5a80d5763e4619f54&redirect_uri=http://localhost:8080/oauth/kakao/callback&response_type=code"
+          href="https://kauth.kakao.com/oauth/authorize?client_id=437bc2fb95b24ca5a80d5763e4619f54&redirect_uri=http://localhost:3000/auth/manager/kakao&response_type=code"
+          //onClick={onKakaoConnect}
+        >
+          {isSignUp ? (
+            <CustomOauthBtn
+              width={width}
+              height={height}
+              radius={radius}
+              text={"카카오 회원가입"}
+              color={COLOR.KAKAO_BROWN}
+              bgColor={COLOR.KAKAO_YELLOW}
+              icon={"kakaoIcon.png"}
+              iconWidth={"40px"}
+            />
+          ) : (
+            <ImgWrapper src="kakao_btn.png" />
+          )}
+        </ButtonWrapper>
+      )}
     </>
   );
 };
--- src/component/login/naverButton.jsx
+++ src/component/login/naverButton.jsx
@@ -2,7 +2,13 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { COLOR } from "./../../constants/colors";
 import CustomOauthBtn from "./CustomOauthBtn";
-const NaverButton = ({ isSignUp = false, width, height, radius }) => {
+const NaverButton = ({
+  type = "student",
+  isSignUp = false,
+  width,
+  height,
+  radius,
+}) => {
   useEffect(() => {
     if (isSignUp) {
       localStorage.setItem("isSignUp", "true");
@@ -10,22 +16,43 @@ const NaverButton = ({ isSignUp = false, width, height, radius }) => {
   }, []);
 
   return (
-    <ButtonWrapper href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=uD5deNWJiPwEgGJY2FO7&redirect_uri=http://localhost:3000/auth/naver&state=1234">
-      {isSignUp ? (
-        <CustomOauthBtn
-          width={width}
-          height={height}
-          radius={radius}
-          text={"네이버 회원가입"}
-          color={COLOR.WHITE}
-          bgColor={COLOR.NAVER_GREEN}
-          icon={"naverIcon.png"}
-          iconWidth={"40px"}
-        />
+    <>
+      {type === "student" ? (
+        <ButtonWrapper href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=uD5deNWJiPwEgGJY2FO7&redirect_uri=http://localhost:3000/auth/naver&state=1234">
+          {isSignUp ? (
+            <CustomOauthBtn
+              width={width}
+              height={height}
+              radius={radius}
+              text={"네이버 회원가입"}
+              color={COLOR.WHITE}
+              bgColor={COLOR.NAVER_GREEN}
+              icon={"naverIcon.png"}
+              iconWidth={"40px"}
+            />
+          ) : (
+            <ImgWrapper src="naver_btn.png" />
+          )}
+        </ButtonWrapper>
       ) : (
-        <ImgWrapper src="naver_btn.png" />
+        <ButtonWrapper href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=uD5deNWJiPwEgGJY2FO7&redirect_uri=http://localhost:3000/auth/manager/naver&state=1234">
+          {isSignUp ? (
+            <CustomOauthBtn
+              width={width}
+              height={height}
+              radius={radius}
+              text={"네이버 회원가입"}
+              color={COLOR.WHITE}
+              bgColor={COLOR.NAVER_GREEN}
+              icon={"naverIcon.png"}
+              iconWidth={"40px"}
+            />
+          ) : (
+            <ImgWrapper src="naver_btn.png" />
+          )}
+        </ButtonWrapper>
       )}
-    </ButtonWrapper>
+    </>
   );
 };
 
