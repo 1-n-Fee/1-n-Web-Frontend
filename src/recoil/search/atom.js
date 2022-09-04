394c92bed27c3992bb1009b27187827ae95b13b8
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-09-05 02:47:24 +0900

    ✨ searchResult, searchDetail atom 생성

---
+++ src/recoil/search/atom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const searchResultAtom = atom({
+  key: "searchResultAtom",
+  default: { isOpen: false, data: [] },
+});
+
+export const searchDetailAtom = atom({
+  key: "searchDetailAtom",
+  default: { isOpen: false, data: {} },
+});
